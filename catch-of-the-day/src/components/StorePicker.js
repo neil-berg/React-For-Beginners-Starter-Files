5bfdf6450a13434863e339002c85db07ca345ead
Neil Berg <neil.berg14@gmail.com>
2019-02-09 18:30:19 -0800

    submit event

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    e.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     // return <p>I am the store pickerrrr!</p>
     return (
-    <React.Fragment>
-      <p>Fish</p> 
-      <form action="" className="store-selector">
-        <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name"/>
-        <button type="submit">Visit Store -></button>
-      </form>
-    </React.Fragment>
-    )
+      <React.Fragment>
+        <p>Fish</p>
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please enter a store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store -></button>
+        </form>
+      </React.Fragment>
+    );
   }
 }
 
