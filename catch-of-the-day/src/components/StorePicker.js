eb298ff3a28d898544c6e6958d690a268e6f51d4
Neil Berg <neil.berg14@gmail.com>
2019-02-24 18:46:15 -0800

    with Redirect instead of history

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,26 +1,45 @@
-import React from "react";
-import { getFunName } from "../helpers";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+// import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  myInput = React.createRef();
+  state = { storeName: '', slug: '', toStorePage: false };
+
+  handleStoreName = e => {
+    this.setState({
+      storeName: e.target.value
+    });
+  };
 
   goToStore = e => {
     e.preventDefault();
-    const storeName = this.myInput.current.value;
-    this.props.history.push(`/store/${storeName}`);
+    this.setState(() => ({
+      slug: this.state.storeName.split(' ').join('-'),
+      toStorePage: true
+    }));
+    // const storeName = this.myInput.current.value;
   };
 
   render() {
+    if (this.state.toStorePage) {
+      return <Redirect push to={`/store/${this.state.slug}`} />;
+    }
     return (
       <React.Fragment>
-        <form action="" className="store-selector" onSubmit={this.goToStore}>
+        <form
+          action=""
+          className="store-selector"
+          onSubmit={this.goToStore}
+          onChange={this.handleStoreName}
+        >
           <h2>Please enter a store</h2>
           <input
             type="text"
-            ref={this.myInput}
+            // ref={this.myInput}
             required
             placeholder="Store Name"
-            defaultValue={getFunName()}
+            value={this.state.value}
+            // defaultValue={getFunName()}
           />
           <button type="submit">Visit Store -></button>
         </form>
