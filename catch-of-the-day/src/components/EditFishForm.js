52f2a68e7fcb1119444fb9af57769ff59d6f6b54
Neil Berg <neil.berg14@gmail.com>
2019-02-25 22:48:11 -0800

    initial commit

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class EditFishForm extends React.Component {
+  handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const updatedFish = { ...this.props.fish, [name]: value };
+    this.props.updateFish(this.props.index, updatedFish);
+  };
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          type="text"
+          name="name"
+          value={this.props.fish.name}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="price"
+          value={this.props.fish.price}
+          onChange={this.handleChange}
+        />
+        <select type="text" name="status" value={this.props.fish.text}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea
+          type="text"
+          name="desc"
+          value={this.props.fish.desc}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          value={this.props.fish.image}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
