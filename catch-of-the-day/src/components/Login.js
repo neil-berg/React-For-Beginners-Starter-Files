cd849358ad4dce7478a81093dadfe2709221a75f
Neil Berg <neil.berg14@gmail.com>
2019-02-26 19:07:28 -0800

    initial commit

---
+++ catch-of-the-day/src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Login = ({ authenticate }) => (
+  <nav className="login">
+    <h2>Inventory</h2>
+    <p>Sign in to manage your store's inventory</p>
+    <button className="github" onClick={() => authenticate('Github')}>
+      Log In With Github
+    </button>
+    <button className="facebook" onClick={() => authenticate('Facebook')}>
+      Log In With Facebook
+    </button>
+    <button className="twitter" onClick={() => authenticate('Twitter')}>
+      Log In With Twitter
+    </button>
+  </nav>
+);
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+};
+
+export default Login;
